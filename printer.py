d8a6df69900ac5a22933f82ac2a4903806a27e86
Br4v3_H3r0 <virusking702@gmail.com>
2020-08-19 18:31:01 +0000

    to print out ascii for a given range

---
+++ printer.py
@@ -0,0 +1,12 @@
+print("Start limit:")
+n=int(input())          #to input start of the ASCII list
+
+print("End Limit:")
+i=input()               #to input end of the ASCII list
+
+#print("\n")
+#print("NUM  ASCII")
+
+while n!=(int(i)+1):        #ASCII printer module
+   print(chr(n), end="")
+   n=n+1
